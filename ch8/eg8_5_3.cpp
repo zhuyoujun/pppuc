190424314ccd2f2a15198a964f290c5308ef4cca
zhuyoujun <zhuyoujun0513@163.com>
2016-01-20 22:57:34 +0800

    update

---
+++ ch8/eg8_5_3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int f(int x)
+{
+	x = x + 1;
+	return x;
+}
+
+int main()
+{
+	int xx = 0;
+	cout << f(xx) << endl;
+	cout << xx << endl;
+
+	int yy = 7;
+	cout << f(yy) << endl;
+	cout << yy << endl;
+
+}
